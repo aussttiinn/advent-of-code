efd80c312c8aa3d17f10b86da6bb4299bca0ee29
Austin Puia <puia.austin@gmail.com>
2024-12-01 19:17:29 -0500

    creating shell for running and testing the solutions

---
+++ aoc-java/src/main/java/org/aoc/Main.java
@@ -0,0 +1,12 @@
+package org.aoc;
+import org.aoc.model.year2024.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    public static Logger logger = LoggerFactory.getLogger("AOC_MAIN");
+
+    public static void main(String[] args) {
+        logger.info("2024_DAY1_PART1: {}", new D1().run());
+    }
+}
