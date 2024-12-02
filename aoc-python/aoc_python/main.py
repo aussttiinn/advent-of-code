7ad8c277d25940ed0c44f8ac5749065bb02035ff
Austin Puia <puia.austin@gmail.com>
2024-12-01 21:12:10 -0500

    doing it in python

---
+++ aoc-python/aoc_python/main.py
@@ -0,0 +1,4 @@
+from model import solutions as s
+
+if __name__ == "__main__":
+    print(s.Day1Part1().run())
---
+++ aoc-python/aoc_python/model/solutions.py
@@ -0,0 +1,48 @@
+import os
+import re
+import json
+import math
+from enum import Enum
+import logging
+from abc import ABC, abstractmethod
+from typing import Any
+
+class Solution(ABC):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    class InputFormat(Enum):
+        txt = "txt"
+        json = "json"
+
+    def __load_input(self, format:InputFormat=InputFormat.txt):
+        file_name = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "resources", f"{self.year}_{self.day}_{self.part}.{format.value}"))
+        with open(file_name) as f:
+            if format == self.InputFormat.json:
+                data = json.loads(f)
+            elif format == self.InputFormat.txt:
+                data = [s.strip() for s in f.readlines()]
+            else:
+                self.logger.error(f"Format ``{format}`` is not supported.")
+        return data  
+            
+    def __init__(self, year:int, day:int, part:int): 
+        self.year, self.day, self.part = year, day, part
+        self.input = self.__load_input(format=Solution.InputFormat.txt)
+
+    @abstractmethod
+    def run(self) -> Any:
+        pass
+        
+class Day1Part1(Solution):
+    def __init__(self):
+        super().__init__(2024, 1, 1)
+
+    def run(self):
+        left = [int(re.split("\\s+", s)[0]) for s in self.input]
+        right = [int(re.split("\\s+", s)[1]) for s in self.input]
+        left.sort()
+        right.sort()
+
+        return sum([abs(left[i]-right[i]) for i in range(len(left))])
+
