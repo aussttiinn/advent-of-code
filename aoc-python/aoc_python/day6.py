d100bde86fa01238aea69f1adfa8b8d398fb9b70
aussttiinn <puia.austin@gmail.com>
2024-12-10 14:47:59 -0500

    updating for day6

---
+++ aoc-python/aoc_python/day6.py
@@ -0,0 +1,119 @@
+from model import Matrix, Input, Directions
+from enum import Enum
+
+input = Input.read(2024, 6, test_data=False)
+
+class GuardPositions(Enum):
+    """The GuardPositions is facing..."""
+    LEFT = "<"
+    RIGHT = ">"
+    UP = "^"
+    DOWN = "v" 
+
+    ANY = [LEFT, RIGHT, UP, DOWN]
+
+    @classmethod
+    def fromItem(cls, item):
+        if item == cls.LEFT.value:
+            return cls.LEFT
+        if item == cls.RIGHT.value:
+            return cls.RIGHT
+        if item == cls.UP.value:
+            return cls.UP
+        if item == cls.DOWN.value:
+            return cls.DOWN
+        raise ValueError(f"Not a valid guard position. {item}")
+    
+    @classmethod
+    def fromDirection(cls, dir: Directions):
+        if dir == Directions.WEST:
+            return cls.LEFT
+        if dir == Directions.EAST: 
+            return cls.RIGHT
+        if dir == Directions.NORTH: 
+            return cls.UP
+        if dir == Directions.SOUTH:
+            return cls.DOWN
+        raise ValueError(f"Not a valid guard position ``{dir.name}``")
+    
+    def toDirection(self):
+        if self == GuardPositions.LEFT:
+            return Directions.WEST
+        if self == GuardPositions.RIGHT:
+            return Directions.EAST
+        if self == GuardPositions.UP:
+            return Directions.NORTH
+        if self == GuardPositions.DOWN:
+            return Directions.SOUTH
+        
+    def rotateDirection(self):
+        """Rotate clockwise (right turn)."""
+        d = self.toDirection()
+        if d == Directions.NORTH:
+            return Directions.EAST
+        if d == Directions.EAST:
+            return Directions.SOUTH
+        if d == Directions.SOUTH:
+            return Directions.WEST
+        if d == Directions.WEST:
+            return Directions.NORTH
+
+class Lab(Matrix):
+    def findGuardPositions(self, positions: list[GuardPositions] = GuardPositions.ANY) -> tuple:
+        """Finds the FIRST occurrence of an item, starting top-to-bottom, left-to-right."""
+        i, j = self.size
+        for idx in range(i):
+            for jdx in range(j):
+                e = self.getElement(idx, jdx)
+                if e in positions.value:
+                    return (GuardPositions.fromItem(e), (idx, jdx))
+        return None, (-1, -1)
+
+class Guard: 
+    def __init__(self, lab: Lab):
+        self.lab = lab
+        x, y = lab.findGuardPositions(GuardPositions.ANY)
+        self.start_guardpos: GuardPositions = x
+        self.start_loc = y
+        self.curr_loc = self.start_loc
+        self.curr_pos: GuardPositions = self.start_guardpos
+    
+    def move(self, d: Directions) -> bool:
+        i, j = self.curr_loc
+        adj = self.lab.getAdjacent(i, j, [d])
+        if adj:
+            new_i, new_j = adj[0]
+            if self.lab.getElement(new_i, new_j) == "#":
+                return self.move(self.curr_pos.rotateDirection())
+            self.curr_loc = (new_i, new_j)
+            self.curr_pos = GuardPositions.fromDirection(d)
+            self.lab.arr[i][j], self.lab.arr[new_i][new_j] = self.lab.arr[new_i][new_j], self.lab.arr[i][j]
+            self.lab.arr[new_i][new_j] = self.curr_pos.value
+            return True
+        return False
+
+    def walk(self):
+        """Loop over moves, performing patrols."""
+        visited = set()
+        visited.add(self.curr_loc) 
+        while True:
+            d = self.curr_pos.toDirection()
+            keepGoing = self.move(d)
+            visited.add(self.curr_loc)
+            print(f"Moved to {self.curr_loc}, facing {self.curr_pos.name}")
+            if not keepGoing:
+                break
+        return visited
+
+def part1():
+    l = Lab([list(line) for line in input])
+    g = Guard(l)
+
+    visited = g.walk()
+    print(len(visited))
+
+
+
+if __name__ == "__main__": 
+    part1()
+
--- aoc-python/aoc_python/model.py
+++ aoc-python/aoc_python/model.py
@@ -12,12 +12,46 @@ class Directions:
     
     ALL = [NORTH, SOUTH, EAST, WEST, NORTHEAST, 
                 NORTHWEST, SOUTHEAST, SOUTHWEST]
+    
+    NO_DIAGONAL = [NORTH, SOUTH, EAST, WEST]
 
 class Input:
     BASE_DIR = os.path.join(os.path.dirname(__file__), "resources")
 
     @classmethod
-    def read(cls, year:int, day:int) -> list[str]:
-        with open(os.path.join(cls.BASE_DIR, f"{year}_{day}.txt")) as f:
+    def read(cls, year:int, day:int, test_data:bool=False) -> list[str]:
+        path = f"{year}_{day}_test.txt" if test_data else f"{year}_{day}.txt"
+        with open(os.path.join(cls.BASE_DIR, path)) as f:
             return [line.strip() for line in f.readlines()]
+
+class Matrix:
+    def _isRectangular(self, arr:list[list]):
+        if not all(len(arr[i])==len(arr) for i in arr):
+            raise Exception("Only rectangular matrices allowed.")
+
+    def __init__(self, arr:list[list]):
+        self.arr=arr
+        self.size=(len(arr),len(arr[0]))
     
+    def _inBounds(self, i, j) -> bool:
+        return (0<=i<self.size[0]) and (0<=j<self.size[1])
+
+    def getElement(self, i, j, default=None): 
+        if self._inBounds(i, j):
+            return self.arr[i][j]
+        return default
+
+    def getAdjacent(self, i, j, directions:list) -> list: 
+        if not self._inBounds(i, j):
+            return []
+        
+        adj = []        
+        for d in directions: 
+            row, col = i+d[0], j+d[1]
+            if self._inBounds(row, col):
+                adj.append((row, col))
+        return adj
+
+    def prettyPrint(self):
+        for row in self.arr:
+            print(" ".join(f"{element:4}" for element in row)) 
